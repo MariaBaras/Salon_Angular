f4efc21e6d8d2189ab65c68545eaced01c863d25
Maria <baras.maria01@gmail.com>
2025-11-22 10:34:18 +0300

    النسخة الأولى من مشروع صالون ماريا

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Salon';
+  title = 'Salon';//صالون ماريا للتجميل مثلاً
 }
